42195a34867d56f30df7da7123e32815e2dbf1f0
OscarOLopez <99523786+OscarOLopez@users.noreply.github.com>
2024-11-12 19:43:13 -0600

    Add files via upload

---
+++ Codigo5.java
@@ -0,0 +1,40 @@
+//Evalua un número ingresado por el usuario para decirle si es un numero afortunado o no lo es, basado en 4 numeros preasignados
+package com.revisioncodigo5;
+
+import java.util.Scanner; //importamos scanner
+
+public class Codigo5 {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in); // Corregimos el scanner y renombramos
+
+		System.out.print("Introduce un número: ");
+		String ni = scanner.nextLine(); // ni se convirtió de String a Int.
+		int c = Integer.parseInt(ni);
+		int original = c; // Añadimos una nueva variable para guardar le valor original de "c" y evitar que nos devuela 0.
+
+
+		int afo = 0;
+		int noAfo = 0; // COlocamos el ";"
+
+		while (c > 0) {
+		int digito = (c % 10); // Eliminamos el int, pues "ni" ya es un entero
+		if (digito == 3 || digito == 7 || digito == 8 || digito == 9) {
+		afo++;
+		} else {
+		noAfo++;
+		}
+
+		c /= 10; // Trasladamos c afuera del else.
+
+		}
+
+		if (afo > noAfo) {
+		System.out.println("El " + original + " es un número afortunado.");
+		} else {
+		System.out.println("El " + original + " no es un número afortunado.");
+		}
+		scanner.close();
+		}
+	
+}
